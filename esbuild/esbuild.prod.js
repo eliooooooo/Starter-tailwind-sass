f95c177dcb679d924494f6a1dd216368bd123fae
Eliott Burkle <79277244+eliooooooo@users.noreply.github.com>
2024-01-14 23:48:11 +0100

    Update esbuild.prod.js, add alternative

--- esbuild/esbuild.prod.js
+++ esbuild/esbuild.prod.js
@@ -28,3 +28,48 @@ esbuild.build({
 })
 .then(result => { console.log('Js & css minifiés !!') })
 .catch(() => process.exit(1));
+
+// Pour un fichier compilé pour chaque js :
+
+// const fs = require('fs');
+// const esbuild = require("esbuild");
+// const postCssPlugin = require("esbuild-style-plugin");
+
+// fs.readdir('./public/src/js', (err, files) => {
+//   if(err) {
+//     console.error("erreur de lecture du dossier ./public/src/js");
+//     process.exit(1);
+//   }
+
+//   const entryPoints = files
+//     .filter(file => file.endsWith('.js'))
+//     .map(file => `./public/src/js/${file}`);
+
+//   esbuild.build({
+//     logLevel: "debug",
+//     entryPoints: entryPoints,
+//     outdir: "dist",
+//     bundle: true,
+//     minify: true, 
+//     loader: {
+//       // ".svg": "file",
+//       // ".otf": "file",
+//       // ".eot": "file",
+//       // ".woff": "file",
+//       // ".woff2": "file"
+//     },
+//     plugins: [
+//       postCssPlugin({
+//         postcss: {
+//           plugins: [
+//             require("postcss-import"),
+//             require("tailwindcss/nesting"),
+//             require("tailwindcss"),
+//             require("autoprefixer")],
+//         },
+//       }),
+//     ],
+//   })
+//   .then(result => { console.log('Js & css minifiés !!') })
+//   .catch(() => process.exit(1));
+// });
