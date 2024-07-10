2bb1c34db88e725d1b4371561d1d257fcc900672
Eliott Burkle <79277244+eliooooooo@users.noreply.github.com>
2024-07-10 22:05:09 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
             'md': '768px',
             'lg': '1024px',
             'xl': '1280px',
-            '2xl': '1536px',
+            '2xl': '1440px',
         },
     },
     plugins: [],
